4199fc9abf264277c62ad34c78e1862789ddc6e4
lkdvos <lukas.devos@ugent.be>
2023-05-30 14:36:19 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.34

---
+++ src/BlockTensorKit.jl
@@ -0,0 +1,5 @@
+module BlockTensorKit
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using BlockTensorKit
+using Test
+
+@testset "BlockTensorKit.jl" begin
+    # Write your tests here.
+end
