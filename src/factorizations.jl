803b733fbd6bd340b47868f9703f1e70686e6106
lkdvos <lukas.devos@ugent.be>
2024-09-09 18:35:46 +0200

    Make factorizations more generic

--- src/factorizations.jl
+++ src/factorizations.jl
@@ -1,10 +1,15 @@
 using TensorKit: QR, QRpos, QL, QLpos, SVD, SDD, Polar, LQ, LQpos, RQ, RQpos
 
-function TK.leftorth!(t::BlockTensorMap;
-                      alg::Union{QR,QRpos,QL,QLpos,SVD,SDD,Polar}=QRpos(),
-                      atol::Real=zero(float(real(scalartype(t)))),
-                      rtol::Real=(alg ∉ (SVD(), SDD())) ? zero(float(real(scalartype(t)))) :
-                                 eps(real(float(one(scalartype(t))))) * iszero(atol))
+function TK.leftorth!(
+    t::AbstractBlockTensorMap;
+    alg::Union{QR,QRpos,QL,QLpos,SVD,SDD,Polar}=QRpos(),
+    atol::Real=zero(float(real(scalartype(t)))),
+    rtol::Real=if (alg ∉ (SVD(), SDD()))
+        zero(float(real(scalartype(t))))
+    else
+        eps(real(float(one(scalartype(t))))) * iszero(atol)
+    end,
+)
     InnerProductStyle(t) === EuclideanProduct() || throw_invalid_innerproduct(:leftorth!)
     if !iszero(rtol)
         atol = max(atol, rtol * norm(t))
@@ -34,8 +39,8 @@ function TK.leftorth!(t::BlockTensorMap;
         W = ProductSpace(V)
     end
 
-    Q = BlockTensorMap{scalartype(t)}(undef, codomain(t) ← W)
-    R = BlockTensorMap{scalartype(t)}(undef, W ← domain(t))
+    Q = similar(t, codomain(t) ← W)
+    R = similar(t, W ← domain(t))
 
     for c in blocksectors(domain(t))
         block(Q, c) .= Qdata[c]
@@ -45,12 +50,16 @@ function TK.leftorth!(t::BlockTensorMap;
     return Q, R
 end
 
-function TK.rightorth!(t::BlockTensorMap;
-                       alg::Union{LQ,LQpos,RQ,RQpos,SVD,SDD,Polar}=LQpos(),
-                       atol::Real=zero(float(real(scalartype(t)))),
-                       rtol::Real=(alg ∉ (SVD(), SDD())) ?
-                                  zero(float(real(scalartype(t)))) :
-                                  eps(real(float(one(scalartype(t))))) * iszero(atol))
+function TK.rightorth!(
+    t::AbstractBlockTensorMap;
+    alg::Union{LQ,LQpos,RQ,RQpos,SVD,SDD,Polar}=LQpos(),
+    atol::Real=zero(float(real(scalartype(t)))),
+    rtol::Real=if (alg ∉ (SVD(), SDD()))
+        zero(float(real(scalartype(t))))
+    else
+        eps(real(float(one(scalartype(t))))) * iszero(atol)
+    end,
+)
     InnerProductStyle(t) === EuclideanProduct() || throw_invalid_innerproduct(:rightorth!)
     if !iszero(rtol)
         atol = max(atol, rtol * norm(t))
@@ -80,8 +89,8 @@ function TK.rightorth!(t::BlockTensorMap;
         W = ProductSpace(V)
     end
 
-    L = BlockTensorMap{scalartype(t)}(undef, codomain(t) ← W)
-    Q = BlockTensorMap{scalartype(t)}(undef, W ← domain(t))
+    L = similar(t, codomain(t) ← W)
+    Q = similar(t, W ← domain(t))
     for c in blocksectors(codomain(t))
         block(L, c) .= Ldata[c]
         block(Q, c) .= Qdata[c]
@@ -89,11 +98,11 @@ function TK.rightorth!(t::BlockTensorMap;
     return L, Q
 end
 
-function TK.tsvd!(t::BlockTensorMap; trunc=NoTruncation(), p::Real=2, alg=SDD())
+function TK.tsvd!(t::AbstractBlockTensorMap; trunc=NoTruncation(), p::Real=2, alg=SDD())
     return TK._tsvd!(t, alg, trunc, p)
 end
 
-function TK._compute_svddata!(t::BlockTensorMap, alg::Union{SVD,SDD})
+function TK._compute_svddata!(t::AbstractBlockTensorMap, alg::Union{SVD,SDD})
     InnerProductStyle(t) === EuclideanProduct() || throw_invalid_innerproduct(:tsvd!)
     I = sectortype(t)
     A = storagetype(t)
@@ -115,7 +124,7 @@ function TK._compute_svddata!(t::BlockTensorMap, alg::Union{SVD,SDD})
     return Udata, Σdata, Vdata, dims
 end
 
-function TK._empty_svdtensors(t::BlockTensorMap)
+function TK._empty_svdtensors(t::AbstractBlockTensorMap)
     S = spacetype(t)
     A = storagetype(t)
     Ar = similarstoragetype(t, real(scalartype(t)))
@@ -130,7 +139,7 @@ function TK._empty_svdtensors(t::BlockTensorMap)
     return U, Σ, V
 end
 
-function TK._create_svdtensors(t::BlockTensorMap, Udata, Σdata, Vdata, W)
+function TK._create_svdtensors(t::AbstractBlockTensorMap, Udata, Σdata, Vdata, W)
     I = sectortype(t)
     S = spacetype(t)
     A = storagetype(t)
