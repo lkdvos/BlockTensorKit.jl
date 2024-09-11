b4c293a57413a166a4e0f712bf10a21a7d091cf6
lkdvos <lukas.devos@ugent.be>
2024-09-11 16:17:02 +0200

    remove unused code

--- src/tensoroperations.jl
+++ src/tensoroperations.jl
@@ -17,21 +17,6 @@ function TO.tensoradd_type(TC, A::AdjointBlockTensorMap, pA::Index2Tuple, conjA:
     return TO.tensoradd_type(TC, A', adjointtensorindices(A, pA), !conjA)
 end
 
-#
-# function TO.tensoradd_structure(pC::Index2Tuple{N₁,N₂}, A::BlockTensorMap{E,S},
-#                                 conjA::Symbol) where {E,S,N₁,N₂}
-#     if conjA == :N
-#         pC′ = pC
-#         V = space(A)
-#     else
-#         pC′ = TK.adjointtensorindices(A, pC)
-#         V = space(A)'
-#     end
-#     cod = ProductSumSpace{S,N₁}(getindex.(Ref(V), pC′[1]))
-#     dom = ProductSumSpace{S,N₂}(dual.(getindex.(Ref(V), pC′[2])))
-#     return dom → cod
-# end
-#
 function TO.tensorcontract_type(
     TC,
     A::AbstractBlockTensorMap,
@@ -111,20 +96,6 @@ function similarblocktype(::Type{A}, ::Type{TT}) where {A,TT}
     return Core.Compiler.return_type(similar, Tuple{A,Type{TT},NTuple{numind(TT),Int}})
 end
 
-#
-# function TO.tensorcontract_structure(pC::Index2Tuple{N₁,N₂}, A::BlockTensorMap,
-#                                      pA::Index2Tuple,
-#                                      conjA::Symbol, B::BlockTensorMap, pB::Index2Tuple,
-#                                      conjB::Symbol) where {N₁,N₂}
-#     spacetype(A) == spacetype(B) || throw(SpaceMismatch("incompatible space types"))
-#     spaces1 = TO.flag2op(conjA).(space.(Ref(A), pA[1]))
-#     spaces2 = TO.flag2op(conjB).(space.(Ref(B), pB[2]))
-#     spaces = (spaces1..., spaces2...)
-#     cod = ProductSumSpace{spacetype(A),N₁}(getindex.(Ref(spaces), pC[1]))
-#     dom = ProductSumSpace{spacetype(A),N₂}(dual.(getindex.(Ref(spaces), pC[2])))
-#     return dom → cod
-# end
-
 # By default, make "dense" allocations
 function TO.tensoralloc(
     ::Type{BT}, structure::TensorMapSumSpace, istemp::Val, allocator=TO.DefaultAllocator()
@@ -143,43 +114,6 @@ function TO.tensorfree!(t::SparseBlockTensorMap, allocator=TO.DefaultAllocator()
     foreach(Base.Fix2(TO.tensorfree!, allocator), nonzero_values(t))
     return nothing
 end
-
-# function TO.tensoradd!(C::BlockTensorMap, pC::Index2Tuple,
-#                        A::BlockTensorMap, conjA::Symbol,
-#                        α::Number, β::Number, backend::Backend...)
-#     argcheck_tensoradd(C.data, pC, A.data)
-#     dimcheck_tensoradd(C.data, pC, A.data)
-#     for IA in eachindex(IndexCartesian(), A)
-#         IC = CartesianIndex(TupleTools.getindices(IA.I, linearize(pC)))
-#         C[IC] = tensoradd!(C[IC], pC, A[IA], conjA, α, β, backend...)
-#     end
-#     return C
-# end
-#
-# function TO.tensorcontract!(C::BlockTensorMap, pC::Index2Tuple,
-#                             A::BlockTensorMap, pA::Index2Tuple, conjA::Symbol,
-#                             B::BlockTensorMap, pB::Index2Tuple, conjB::Symbol,
-#                             α::Number, β::Number, backend::Backend...)
-#     argcheck_tensorcontract(parent(C), pC, parent(A), pA, parent(B), pB)
-#     dimcheck_tensorcontract(parent(C), pC, parent(A), pA, parent(B), pB)
-#     ipC = invperm(linearize(pC))
-#     for IC in eachindex(IndexCartesian(), C)
-#         β′ = β
-#         IAB = TupleTools.getindices(IC.I, ipC)
-#         IAo = TupleTools.getindices(IAB, ntuple(identity, TO.numout(pA)))
-#         IBo = TupleTools.getindices(IAB, ntuple(i -> i + TO.numout(pA), TO.numin(pB)))
-#         for Ic in Iterators.product(axes.(Ref(A), pA[2])...)
-#             IA = CartesianIndex(TupleTools.getindices(linearize((IAo, Ic)),
-#                                                       invperm(linearize(pA))))
-#             IB = CartesianIndex(TupleTools.getindices(linearize((Ic, IBo)),
-#                                                       invperm(linearize(pB))))
-#             C[IC] = tensorcontract!(C[IC], pC, A[IA], pA, conjA, B[IB], pB, conjB, α, β′,
-#                                     backend...)
-#             β′ = one(β)
-#         end
-#     end
-#     return C
-# end
 
 function TK.trace_permute!(
     tdst::BlockTensorMap,
@@ -216,77 +150,6 @@ function TK.trace_permute!(
     return tdst
 end
 
-# function TO.tensortrace!(C::BlockTensorMap{E,S}, pC::Index2Tuple,
-#                          A::BlockTensorMap{E,S},
-#                          pA::Index2Tuple,
-#                          conjA::Symbol, α::Number, β::Number,
-#                          backend::Backend...) where {E,S}
-#     argcheck_tensortrace(C.data, pC, A.data, pA)
-#     dimcheck_tensortrace(C.data, pC, A.data, pA)
-#
-#     scale!(C, β)
-#     β′ = One()
-#     for IA in eachindex(IndexCartesian(), A)
-#         IAc1 = CartesianIndex(getindices(IA.I, pA[1]))
-#         IAc2 = CartesianIndex(getindices(IA.I, pA[2]))
-#         IAc1 == IAc2 || continue
-#
-#         IC = CartesianIndex(getindices(IA.I, linearize(pC)))
-#         C[IC] = tensortrace!(C[IC], pC, A[IA], pA, conjA, α, β′, backend...)
-#     end
-#     return C
-# end
-
-# TO.tensorstructure(t::BlockTensorMap) = space(t)
-# function TO.tensorstructure(t::BlockTensorMap, iA::Int, conjA::Symbol)
-#     return conjA == :N ? space(t, iA) : conj(space(t, iA))
-# end
-#
-# function TO.tensorcontract_structure(pC::Index2Tuple{N₁,N₂},
-#                                      A::BlockTensorMap{E,S}, pA::Index2Tuple, conjA::Symbol,
-#                                      B::BlockTensorMap{E,S}, pB::Index2Tuple,
-#                                      conjB::Symbol) where {E,S,N₁,N₂}
-#     spaces1 = TO.flag2op(conjA).(space.(Ref(A), pA[1]))
-#     spaces2 = TO.flag2op(conjB).(space.(Ref(B), pB[2]))
-#     spaces = (spaces1..., spaces2...)
-#     cod = ProductSumSpace{S,N₁}(getindex.(Ref(spaces), pC[1]))
-#     dom = ProductSumSpace{S,N₂}(dual.(getindex.(Ref(spaces), pC[2])))
-#     return dom → cod
-# end
-# function TO.tensorcontract_structure(pC::Index2Tuple{N₁,N₂},
-#                                      A::BlockTensorMap{E,S}, pA::Index2Tuple, conjA::Symbol,
-#                                      B::AbstractTensorMap{E,S}, pB::Index2Tuple,
-#                                      conjB::Symbol) where {E,S,N₁,N₂}
-#     spaces1 = TO.flag2op(conjA).(space.(Ref(A), pA[1]))
-#     spaces2 = TO.flag2op(conjB).(space.(Ref(B), pB[2]))
-#     spaces = (spaces1..., spaces2...)
-#     cod = ProductSumSpace{S,N₁}(getindex.(Ref(spaces), pC[1]))
-#     dom = ProductSumSpace{S,N₂}(dual.(getindex.(Ref(spaces), pC[2])))
-#     return dom → cod
-# end
-# function TO.tensorcontract_structure(pC::Index2Tuple{N₁,N₂},
-#                                      A::AbstractTensorMap{E,S}, pA::Index2Tuple,
-#                                      conjA::Symbol,
-#                                      B::BlockTensorMap{E,S}, pB::Index2Tuple,
-#                                      conjB::Symbol) where {E,S,N₁,N₂}
-#     spaces1 = TO.flag2op(conjA).(space.(Ref(A), pA[1]))
-#     spaces2 = TO.flag2op(conjB).(space.(Ref(B), pB[2]))
-#     spaces = (spaces1..., spaces2...)
-#     cod = ProductSumSpace{S,N₁}(getindex.(Ref(spaces), pC[1]))
-#     dom = ProductSumSpace{S,N₂}(dual.(getindex.(Ref(spaces), pC[2])))
-#     return dom → cod
-# end
-#
-# function TO.checkcontractible(tA::BlockTensorMap{E,S}, iA::Int, conjA::Symbol,
-#                               tB::BlockTensorMap{E,S}, iB::Int, conjB::Symbol,
-#                               label) where {E,S}
-#     sA = TO.tensorstructure(tA, iA, conjA)'
-#     sB = TO.tensorstructure(tB, iB, conjB)
-#     sA == sB ||
-#         throw(SpaceMismatch("incompatible spaces for $label: $sA ≠ $sB"))
-#     return nothing
-# end
-
 # PlanarOperations
 # ----------------
 
@@ -307,37 +170,6 @@ function TK.BraidingTensor(V1::SumSpace{S}, V2::SumSpace{S}) where {S}
     end
     return tdst
 end
-
-# function TK.planaradd!(C::BlockTensorMap,
-#                        A::BlockTensorMap, p::Index2Tuple{N₁,N₂},
-#                        α::Number, β::Number,
-#                        backend::AbstractBackend, allocator)
-#     
-#     scale!(C, β)
-#     indCinA = linearize(p)
-#     for (IA, v) in nonzero_pairs(A)
-#         IC = CartesianIndex(TupleTools.getindices(IA.I, indCinA))
-#         C[IC] = TK.planaradd!(C[IC], v, p, α, One())
-#     end
-#     return C
-# end
-
-# function TK.planaradd!(C::AbstractTensorMap, A::BlockTensorMap,
-#                        p::Index2Tuple{N₁,N₂}, α::Number, β::Number,
-#                        backend::AbstractBackend, allocator) where {E,S,N₁,N₂}
-#     C′ = convert(BlockTensorMap, C)
-#     TK.planaradd!(C′, A, p, α, β, backend...)
-#     return C
-# end
-
-# function TK.planaradd!(C::BlockTensorMap{E,S,N₁,N₂},
-#                        A::AbstractTensorMap{E,S},
-#                        p::Index2Tuple{N₁,N₂},
-#                        α::Number,
-#                        β::Number,
-#                        backend::Backend...) where {E,S,N₁,N₂}
-#     return TK.planaradd!(C, convert(BlockTensorMap, A), p, α, β, backend...)
-# end
 
 # function TK.planartrace!(C::BlockTensorMap,
 #                          A::BlockTensorMap,
@@ -440,81 +272,3 @@ end
 #
 #     return C
 # end
-
-# methods for automatically working with TensorMap - BlockTensorMaps
-# ------------------------------------------------------------------------
-
-# trick ambiguities by using Union instead of AbstractTensorMap
-const AnyTM = Union{TensorMap,TensorKit.AdjointTensorMap}
-
-for (T1, T2) in (
-    (:AnyTM, :BlockTensorMap),
-    (:BlockTensorMap, :AnyTM),
-    (:BlockTensorMap, :BlockTensorMap),
-    (:AnyTM, :AnyTM),
-)
-    if T1 !== :AnyTM && T2 !== :AnyTM
-        # @eval function TO.tensorcontract!(C::AnyTM, A::$T1, pA::Index2Tuple,
-        #                                   conjA::Symbol, B::$T2, pB::Index2Tuple,
-        #                                   conjB::Symbol, pAB::Index2Tuple, α::Number,
-        #                                   β::Number, backend::AbstractBackend, allocator)
-        #     C′ = convert(BlockTensorMap, C)
-        #     tensorcontract!(C′, A, pA, conjA, B, pB, conjB, pAB, α, β, backend, allocator)
-        #     return C
-        # end
-        #
-        # @eval function TK.planarcontract!(C::AnyTM,
-        #                                   A::$T1, pA::Index2Tuple,
-        #                                   B::$T2, pB::Index2Tuple,
-        #                                   pAB::Index2Tuple, α::Number, β::Number,
-        #                                   backend::AbstractBackend, allocator)
-        #     C′ = convert(BlockTensorMap, C)
-        #     TK.planarcontract!(C′, A, pA, B, pB, pAB, α, β, backend, allocator)
-        #     return C
-        # end
-
-        # @eval function TO.checkcontractible(tA::$T1, iA::Int, conjA::Symbol,
-        #                                     tB::$T2, iB::Int, conjB::Symbol,
-        #                                     label)
-        #     sA = TO.tensorstructure(tA, iA, conjA)'
-        #     sB = TO.tensorstructure(tB, iB, conjB)
-        #     sA == sB ||
-        #         throw(SpaceMismatch("incompatible spaces for $label: $sA ≠ $sB"))
-        #     return nothing
-        # end
-    end
-
-    # if T1 !== T2
-    #     @eval function TO.tensorcontract_type(TC, pC, A::$T1, pA, conjA, B::$T2, pB, conjB)
-    #         return TO.tensorcontract_type(TC, pC, convert(BlockTensorMap, A), pA, conjA,
-    #                                       convert(BlockTensorMap, B), pB, conjB)
-    #     end
-    # end
-
-    if !(T1 === :BlockTensorMap && T2 === :BlockTensorMap)
-        # @eval function TO.tensorcontract!(C::BlockTensorMap,
-        #                                   A::$T1, pA::Index2Tuple, conjA::Symbol,
-        #                                   B::$T2, pB::Index2Tuple, conjB::Symbol,
-        #                                   pAB::Index2Tuple, α::Number, β::Number,
-        #                                   backend::AbstractBackend, allocator)
-        #     return TO.tensorcontract!(C,
-        #                               convert(BlockTensorMap, A), pA, conjA,
-        #                               convert(BlockTensorMap, B), pB, conjB,
-        #                               pAB, α, β,
-        #                               backend, allocator)
-        # end
-        #
-        # @eval function TK.planarcontract!(C::BlockTensorMap,
-        #                                   A::$T1, pA::Index2Tuple,
-        #                                   B::$T2, pB::Index2Tuple,
-        #                                   pAB::Index2Tuple, α::Number, β::Number,
-        #                                   backend::AbstractBackend, allocator)
-        #     return TK.planarcontract!(C, convert(BlockTensorMap, A), pA,
-        #                               convert(BlockTensorMap, B), pB,
-        #                               pAB, α, β,
-        #                               backend, allocator)
-        # end
-    end
-end
-
-# TODO: similar for tensoradd!, tensortrace!, planaradd!, planartrace!
