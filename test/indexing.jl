702e1a65127ab87b7e911b28aa052808e04df355
Lukas Devos <ldevos98@gmail.com>
2024-10-30 11:05:06 -0400

    Improve testing

--- test/indexing.jl
+++ test/indexing.jl
@@ -1,4 +1,5 @@
 using BlockTensorKit
+using BlockTensorKit: sprand
 using Test
 using TensorKit
 
@@ -23,6 +24,38 @@ blockt2 = blockt[1, [1, 3], 1]
 @test size(blockt2) == (1, 2, 1)
 blockt3 = @inferred blockt[[1], [1],1]
 @test blockt3 isa BlockTensorMap
+@test length(blockt3) == 1
+
+# invalid indexing
+@test_throws MethodError blockt[:]
+@test_throws MethodError blockt[[1]]
+
+
+blockt = sprand(V ⊗ V ⊗ V, 0.5)
+
+# scalar indexing
+@test @inferred(blockt[1]) isa TensorMap
+@test @inferred(blockt[1,1,1]) isa TensorMap
+@test @inferred(blockt[CartesianIndex(1, 1, 1)]) isa TensorMap
+
+# colon indexing
+blockt2 = @inferred blockt[:, :, :]
+@test blockt2 == blockt
+nnz = nonzero_length(blockt)
+for I in eachindex(blockt)
+    if I in nonzero_keys(blockt)
+        @test blockt[I] === blockt[I]
+    else
+        @test norm(blockt[I]) == 0
+        @test nonzero_length(blockt) == nnz
+    end
+end
+
+@test size(@inferred(blockt[1, :, 1])) == (1, 3, 1) 
+blockt2 = blockt[1, [1, 3], 1]
+@test size(blockt2) == (1, 2, 1)
+blockt3 = @inferred blockt[[1], [1],1]
+@test blockt3 isa SparseBlockTensorMap
 @test length(blockt3) == 1
 
 # invalid indexing
