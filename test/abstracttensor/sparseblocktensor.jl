923c207cf4f4c413f9f54ba742cddff2f696814c
Lukas Devos <ldevos98@gmail.com>
2024-11-10 14:11:03 -0500

    Add sparse tensoroperations tests

--- test/abstracttensor/sparseblocktensor.jl
+++ test/abstracttensor/sparseblocktensor.jl
@@ -1,1 +1,59 @@
 
+using Test
+using TensorKit
+using BlockTensorKit
+using Random
+using Combinatorics
+
+Vtr = (
+    SumSpace(ℂ^3),
+    SumSpace(ℂ^2, ℂ^2)',
+    SumSpace(ℂ^2, ℂ^2, ℂ^1),
+    SumSpace(ℂ^2, ℂ^2, ℂ^2),
+    SumSpace(ℂ^2, ℂ^3, ℂ^1, ℂ^1)',
+)
+
+for V in (Vtr,)
+    V1, V2, V3, V4, V5 = V
+    @assert V3 * V4 * V2 ≿ V1' * V5' # necessary for leftorth tests
+    @assert V3 * V4 ≾ V1' * V2' * V5' # necessary for rightorth tests
+end
+
+spacelist = (Vtr,)
+scalartypes = (Float64, ComplexF64)
+V = first(spacelist)
+# @testset "Tensors with symmetry: $(TensorKit.type_repr(sectortype(first(V))))" verbose = true failfast=true for V in
+#   spacelist
+I = sectortype(first(V))
+V1, V2, V3, V4, V5 = V
+
+@testset "Basic tensor properties" begin
+    W = V1 ⊗ V2 ⊗ V3 ⊗ V4 ⊗ V5
+    for T in scalartypes
+        t = @inferred sparse(zeros(T, W))
+        @test t isa SparseBlockTensorMap
+        @test @inferred(hash(t)) == hash(deepcopy(t))
+        @test scalartype(t) == T
+        @test iszero(norm(t))
+        @test W == @inferred codomain(t)
+        @test one(W) == @inferred domain(t)
+        @test (W ← one(W)) == @inferred space(t)
+    end
+end
+
+@testset "TensorMap conversion" begin
+    W = V1 ⊗ V2 ⊗ V3 ← V4 ⊗ V5
+    for T in scalartypes
+        t1 = sprand(T, W, 0.5)
+        t2 = sprand(T, W, 0.5)
+        t1′ = @inferred convert(TensorMap, t1)
+        t2′ = @inferred convert(TensorMap, t2)
+        @test norm(t1) ≈ norm(t1′)
+        @test norm(t2) ≈ norm(t2′)
+        @test inner(t1, t2) ≈ inner(t1′, t2′)
+        t1″ = @inferred SparseBlockTensorMap(t1′, W)
+        t2″ = @inferred SparseBlockTensorMap(t2′, W)
+        @test t1 ≈ t1″
+        @test t2 ≈ t2″
+    end
+end
--- test/linalg/tensoroperations.jl
+++ test/linalg/tensoroperations.jl
@@ -14,29 +14,34 @@ Vtr = (
 )
 W = Vtr[1] ⊗ Vtr[2] ⊗ Vtr[3] ← Vtr[4] ⊗ Vtr[5]
 @testset "tensorcopy" begin
-    T = ComplexF64
-    A = rand(T, W)
-    B = convert(TensorMap, A)
-    @tensor C1[4, 5, 1, 3, 2] := A[1, 2, 3, 4, 5]
-    @tensor C2[4, 5, 1, 3, 2] := B[1, 2, 3, 4, 5]
-    @test convert(TensorMap, C1) ≈ C2
+    for T in (Float32, ComplexF32), Asparse in (false, true)
+        A = Asparse ? randn(T, W) : sprand(T, W, 0.5)
+        B = convert(TensorMap, A)
+        @tensor C1[4, 5, 1, 3, 2] := A[1, 2, 3, 4, 5]
+        @tensor C2[4, 5, 1, 3, 2] := B[1, 2, 3, 4, 5]
+        @test convert(TensorMap, C1) ≈ C2
+    end
 end
 ##
 
 @testset "tensoradd" begin
-    for T in (Float32, ComplexF32)
-        A = randn(T, W)
-        B = randn(T, W)
+    for T in (Float32, ComplexF32), Asparse in (false, true), Bsparse in (false, true)
+        A = !Asparse ? randn(T, W) : sprand(T, W, 0.5)
+        B = !Bsparse ? randn(T, W) : sprand(T, W, 0.5)
         α = randn(T)
         @tensor C1[a, b, c, d, e] := A[a, b, c, d, e] + α * B[a, b, c, d, e]
         @tensor C2[a, b, c, d, e] :=
             convert(TensorMap, A)[a, b, c, d, e] + α * convert(TensorMap, B)[a, b, c, d, e]
         @test convert(TensorMap, C1) ≈ C2
 
-        D = randn(T, W)
+        D = !Asparse ? randn(T, W) : sprand(T, W, 0.5)
         E = TensorOperations.tensoralloc_add(T, D, ((3, 2, 1, 5, 4), ()), true, Val(false))
+        if Bsparse
+            E = sparse(E)
+        end
         E = Random.randn!(E)
         @tensor F1[a, b, c, d, e] := E[a, b, c, d, e] + α * conj(D[c, b, a, e, d])
+
         @tensor F2[a, b, c, d, e] :=
             convert(TensorMap, E)[a, b, c, d, e] +
             α * conj(convert(TensorMap, D)[c, b, a, e, d])
@@ -64,16 +69,22 @@ end
 ##
 
 @testset "tensorcontract" begin
-    for T in (Float64, ComplexF64)
-        A = randn(T, W[1] ⊗ W[2] ⊗ W[3] ← W[1] ⊗ W[4])
-        B = randn(T, W[3]' ⊗ W[5] ← W[2] ⊗ W[1])
+    for T in (Float64, ComplexF64), Asparse in (false, true), Bsparse in (false, true)
+        WA = W[1] ⊗ W[2] ⊗ W[3] ← W[1] ⊗ W[4]
+        WB = W[3]' ⊗ W[5] ← W[2] ⊗ W[1]
+        A = Asparse ? sprand(T, WA, 0.5) : rand(T, WA)
+        B = Bsparse ? sprand(T, WB, 0.5) : rand(T, WB)
         @tensor C1[a, g, e, d, f] := A[a, b, c, d, e] * B[c, f, b, g]
         @tensor C2[a, g, e, d, f] :=
             convert(TensorMap, A)[a, b, c, d, e] * convert(TensorMap, B)[c, f, b, g]
         @test convert(TensorMap, C1) ≈ C2
 
-        D = randn(real(T), W[1] ⊗ W[1] ← W[1])
-        E = randn(T, W[1] ⊗ W[1] ← W[1])
+        D = if Asparse
+            sprand(real(T), W[1] ⊗ W[1] ← W[1], 0.5)
+        else
+            randn(real(T), W[1] ⊗ W[1] ← W[1])
+        end
+        E = Bsparse ? sprand(T, W[1] ⊗ W[1] ← W[1], 0.5) : randn(T, W[1] ⊗ W[1] ← W[1])
         @tensor F1[a, b, c, d, e, f] := D[a, b, c] * conj(E[d, e, f])
         @tensor F2[a, b, c, d, e, f] :=
             convert(TensorMap, D)[a, b, c] * conj(convert(TensorMap, E)[d, e, f])
