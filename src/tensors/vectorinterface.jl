48aada42ac4adf58e5cc0170af3609d1ee538945
Lukas Devos <ldevos98@gmail.com>
2024-11-09 19:50:12 -0500

    VectorInterface tests and fixes

--- src/tensors/vectorinterface.jl
+++ src/tensors/vectorinterface.jl
@@ -3,6 +3,8 @@
 VI.zerovector!(t::AbstractBlockTensorMap) = (zerovector!(parent(t)); t)
 VI.zerovector!(t::SparseBlockTensorMap) = (empty!(t.data); t)
 
+# scale
+# -----
 function VI.scale!(t::AbstractBlockTensorMap, α::Number)
     foreach(Base.Fix2(scale!, α), nonzero_values(t))
     return t
@@ -15,60 +17,28 @@ function VI.scale!(ty::BlockTensorMap, tx::BlockTensorMap, α::Number)
 end
 function VI.scale!(ty::SparseBlockTensorMap, tx::SparseBlockTensorMap, α::Number)
     space(ty) == space(tx) || throw(SpaceMismatch("$(space(ty)) ≠ $(space(tx))"))
+    y_notin_x = setdiff(nonzero_keys(ty), nonzero_keys(tx))
+    x_notin_y = setdiff(nonzero_keys(tx), nonzero_keys(ty))
+    inboth = intersect(nonzero_keys(ty), nonzero_keys(tx))
+
     # remove elements that are not in tx
-    for k in setdiff(nonzero_keys(ty), nonzero_keys(tx))
+    for k in y_notin_x
         delete!(ty.data, k)
     end
     # in-place scale elements that are in both
-    for k in intersect(nonzero_keys(ty), nonzero_keys(tx))
+    for k in inboth
         ty[k] = scale!(ty[k], tx[k], α)
     end
     # new scale for elements in x that are not in y
-    for k in setdiff(nonzero_keys(tx), nonzero_keys(ty))
+    for k in x_notin_y
         ty[k] = scale(tx[k], α)
     end
+
     return ty
 end
-# function VI.scale!(ty::BlockTensorMap, tx::BlockTensorMap,
-#                    α::Number)
-#     space(ty) == space(tx) || throw(SpaceMismatch())
-#     # delete all entries in ty that are not in tx
-#     for I in setdiff(nonzero_keys(ty), nonzero_keys(tx))
-#         delete!(ty.data, I)
-#     end
-#     # in-place scale elements from tx (getindex might allocate!)
-#     for (I, v) in nonzero_pairs(tx)
-#         ty[I] = scale!(ty[I], v, α)
-#     end
-#     return ty
-# end
-# function VI.scale!!(x::BlockTensorMap, α::Number)
-#     return VI.promote_scale(x, α) <: scalartype(x) ? scale!(x, α) : scale(x, α)
-# end
-# function VI.scale!!(y::BlockTensorMap, x::BlockTensorMap,
-#                     α::Number)
-#     return VI.promote_scale(x, α) <: scalartype(y) ? scale!(y, x, α) : scale(x, α)
-# end
 
-# function VI.add(y::BlockTensorMap, x::BlockTensorMap, α::Number,
-#                 β::Number)
-#     space(y) == space(x) || throw(TK.SpaceMisMatch())
-#     T = VI.promote_add(y, x, α, β)
-#     z = zerovector(y, T)
-#     # TODO: combine these operations where possible
-#     scale!(z, y, β)
-#     return add!(z, x, α)
-# end
-# function VI.add!(y::BlockTensorMap, x::BlockTensorMap, α::Number,
-#                  β::Number)
-#     space(y) == space(x) || throw(SpaceMisMatch())
-#     # TODO: combine these operations where possible
-#     scale!(y, β)
-#     for (k, v) in nonzero_pairs(x)
-#         y[k] = add!(y[k], v, α)
-#     end
-#     return y
-# end
+# add
+# ---
 function VI.add!(ty::BlockTensorMap, tx::BlockTensorMap, α::Number, β::Number)
     space(ty) == space(tx) || throw(SpaceMismatch("$(space(ty)) ≠ $(space(tx))"))
     add!(parent(ty), parent(tx), α, β)
@@ -76,9 +46,20 @@ function VI.add!(ty::BlockTensorMap, tx::BlockTensorMap, α::Number, β::Number)
 end
 function VI.add!(ty::SparseBlockTensorMap, tx::SparseBlockTensorMap, α::Number, β::Number)
     space(ty) == space(tx) || throw(SpaceMismatch("$(space(ty)) ≠ $(space(tx))"))
-    for (k, v) in nonzero_pairs(tx)
-        ty[k] = α * ty[k] + β * v
+    y_notin_x = setdiff(nonzero_keys(ty), nonzero_keys(tx))
+    x_notin_y = setdiff(nonzero_keys(tx), nonzero_keys(ty))
+    inboth = intersect(nonzero_keys(ty), nonzero_keys(tx))
+
+    for k in y_notin_x
+        scale!(ty[k], β)
     end
+    for k in x_notin_y
+        ty[k] = scale(tx[k], α)
+    end
+    for k in inboth
+        add!(ty[k], tx[k], α, β)
+    end
+
     return ty
 end
 
--- test/vectorinterface.jl
+++ test/vectorinterface.jl
@@ -1,6 +1,7 @@
-using Test, TestExtras
+using Test
 using TensorKit
 using BlockTensorKit
+using BlockTensorKit: sprand
 using VectorInterface
 
 Vtr = (
@@ -11,25 +12,134 @@ Vtr = (
     SumSpace(ℂ^2, ℂ^2, ℂ^3)',
 )
 
-for V in (Vtr,), k in rand(0:5, 3)
-    W = prod(V[1:k]; init=one(V[1])) ← prod(V[(k + 1):end]; init=one(V[1]))
-    @testset "Trivial ($T)" for T in (Float64, ComplexF64)
-        t = rand(T, W)
-        @test T == @constinferred scalartype(T)
+V = Vtr
 
-        @test @constinferred(norm(t)) isa real(T)
+@testset "VectorInterface $(issparse ? "SparseBlockTensorMap" : "BlockTensorMap")" for issparse in
+                                                                                       (
+    false, true
+)
+    if issparse
+        t = sprand(Float64, *(V[1:3]...) ← *(V[4], V[5]), 0.5)
+        t′ = sprand(Float64, *(V[1:3]...) ← *(V[4], V[5]), 0.5)
+    else
+        t = rand(*(V[1:3]...) ← *(V[4], V[5]))
+        t′ = rand!(similar(t))
+    end
+
+    @testset "scalartype" begin
+        @test Float64 === @inferred scalartype(t)
+    end
+
+    @testset "zerovector" begin
+        tmp = deepcopy(t)
+        t1 = @inferred zerovector(tmp)
+        t2 = @inferred zerovector!(tmp)
+        t3 = @inferred zerovector!!(tmp)
+
+        @test norm(t1) == norm(t2) == norm(t3) == 0
+        @test t1 !== tmp
+        @test t2 === t3 === tmp
+
+        t4 = @inferred zerovector(t, ComplexF64)
+        @test t4 !== t
+        @test scalartype(t4) === ComplexF64
+        @test iszero(norm(t4))
+
+        t5 = @inferred zerovector!!(tmp, Float64)
+        t6 = @inferred zerovector!!(tmp, ComplexF64)
+        @test t5 === tmp
+        @test t6 !== tmp
+    end
+
+    @testset "scale" begin
+        α = randn()
+        β = randn()
+
+        t1 = @inferred scale(t, α)
+        @test norm(t1) ≈ abs(α) * norm(t)
+        @test t1 !== t
+        @test scale(t, α * β) ≈ scale(scale(t, α), β)
+
+        t2 = @inferred scale!!(t1, inv(α))
+        @test t2 === t1
+        @test t2 ≈ t
+
+        t3 = @inferred scale!(t1, α)
+        @test t3 === t1
+        @test t1 ≈ scale(t, α)
+
+        γ = randn(ComplexF64)
+        t4 = zerovector(t)
+        t5 = @inferred scale!!(t4, t, γ)
+        @test t5 !== t
+        @test t5 !== t4
+        @test scalartype(t5) == typeof(γ)
+        @test norm(t5) ≈ norm(t) * abs(γ)
+
+        t6 = zerovector(t, ComplexF64)
+        t7 = zerovector(t6)
+        t8 = @inferred scale!(t6, t, γ)
+        t9 = @inferred scale!!(t7, t, γ)
+        @test t6 === t8
+        @test t7 === t9
+        @test t8 == t9 == t5
+    end
+
+    @testset "add" begin
+        α, β = rand(2)
+        γ, δ = rand(ComplexF64, 2)
+
+        t3 = @inferred add(t, t′)
+        @test t3 == add(t′, t)
+
+        @test add(t, t) ≈ scale(t, 2)
+        @test @inferred add(t, t, α) ≈ scale(t, (1 + α))
+        @test @inferred add(t, t, α, β) ≈ scale(t, (α + β))
+        @test add(t, t′, α, β) ≈ add(scale(t, β), scale(t′, α))
+        @test @inferred add(t, t′, γ, δ) ≈ @inferred add(scale(t, δ), scale(t′, γ))
+
+        tmp = deepcopy(t)
+        t2 = @inferred add!(tmp, t′)
+        @test t2 ≈ add(t, t′)
+        @test t2 === tmp
+        t2 = @inferred add!(deepcopy(t), t′, α)
+        @test t2 ≈ add(t, t′, α)
+        t2 = @inferred add!(deepcopy(t), t′, α, β)
+        @test t2 ≈ add(t, t′, α, β)
+
+        tmp = deepcopy(t)
+        t2 = @inferred add!!(tmp, t′)
+        @test t2 ≈ add(t, t′)
+        @test t2 === tmp
+        t2 = @inferred add!!(deepcopy(t), t′, α)
+        @test t2 ≈ add(t, t′, α)
+        t2 = @inferred add!!(deepcopy(t), t′, α, β)
+        @test t2 ≈ add(t, t′, α, β)
+
+        tmp = deepcopy(t)
+        t3 = @inferred add!!(tmp, t′, γ, One())
+        @test t3 !== tmp
+        @test scalartype(t3) === ComplexF64
+        @test t3 ≈ add(tmp, t′, γ, One())
+    end
+
+    @testset "inner" begin
         @test norm(t)^2 ≈ inner(t, t)
+        @test inner(t, t′) ≈ conj(inner(t′, t))
+        α, β = rand(ComplexF64, 2)
+        t″ = scale(t, α)
+        t‴ = scale(t′, β)
+        @test @inferred inner(t″, t‴) ≈ conj(α) * β * inner(t, t′)
+    end
 
-        α = rand(T)
+    @testset "general linalg" begin
+        α, β = rand(ComplexF64, 2)
         @test (α * α) * t ≈ α * (α * t)
         @test norm(α * t) ≈ abs(α) * norm(t)
         @test t + t ≈ 2 * t
         @test t - t ≈ zero(t)
-        @test -t ≈ -one(T) * t
-
-        t2 = rand(T, W)
-        β = rand(T)
-        @test inner(β * t2, α * t) ≈ conj(β) * α * conj(inner(t, t2))
-        @test inner(t, t2) ≈ conj(inner(t2, t))
+        @test -t ≈ -one(scalartype(t)) * t
+        @test inner(β * t′, α * t) ≈ conj(β) * α * conj(inner(t, t′))
+        @test inner(t, t′) ≈ conj(inner(t′, t))
     end
 end
