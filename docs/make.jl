b4266932a623b270d8cec1eb0225d0f7baca7538
Lukas Devos <ldevos98@gmail.com>
2024-11-29 17:30:00 +0100

    Add docs makefile

---
+++ docs/make.jl
@@ -0,0 +1,26 @@
+using Documenter
+using BlockTensorKit
+
+DocMeta.setdocmeta!(BlockTensorKit, :DocTestSetup, :(using TensorKit, BlockTensorKit); recursive=true)
+
+pages = [
+    "Home" => "index.md",
+    "Manual" => ["SumSpace" => "sumspaces.md",
+        "BlockTensors" => "blocktensors.md"],
+    "Library" => "lib.md",
+]
+
+makedocs(;
+    modules=[BlockTensorKit],
+    sitename="BlockTensorKit.jl",
+    authors="Lukas Devos",
+    warnonly=[:missing_docs, :cross_references],
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", nothing) == "true", mathengine=MathJax(), repolink="https://github.com/lkdvos/BlockTensorKit.jl.git"
+    ),
+    pages=pages,
+    pagesonly=true,
+    repo="github.com/lkdvos/BlockTensorKit.jl.git"
+)
+
+# deploydocs(; repo="github.com/lkdvos/BlockTensorKit.jl.git", push_preview=true)
