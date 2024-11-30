7b7922e83a1086ed80ef2db9357e245c14abd810
Lukas Devos <ldevos98@gmail.com>
2024-11-30 08:24:25 -0500

    Format and enable deploy

--- docs/make.jl
+++ docs/make.jl
@@ -1,12 +1,9 @@
 using Documenter
 using BlockTensorKit
 
-DocMeta.setdocmeta!(BlockTensorKit, :DocTestSetup, :(using TensorKit, BlockTensorKit); recursive=true)
-
 pages = [
     "Home" => "index.md",
-    "Manual" => ["SumSpace" => "sumspaces.md",
-        "BlockTensors" => "blocktensors.md"],
+    "Manual" => ["SumSpace" => "sumspaces.md", "BlockTensors" => "blocktensors.md"],
     "Library" => "lib.md",
 ]
 
@@ -16,11 +13,13 @@ makedocs(;
     authors="Lukas Devos",
     warnonly=[:missing_docs, :cross_references],
     format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", nothing) == "true", mathengine=MathJax(), repolink="https://github.com/lkdvos/BlockTensorKit.jl.git"
+        prettyurls=get(ENV, "CI", nothing) == "true",
+        mathengine=MathJax(),
+        repolink="https://github.com/lkdvos/BlockTensorKit.jl.git",
     ),
     pages=pages,
     pagesonly=true,
-    repo="github.com/lkdvos/BlockTensorKit.jl.git"
+    repo="github.com/lkdvos/BlockTensorKit.jl.git",
 )
 
-# deploydocs(; repo="github.com/lkdvos/BlockTensorKit.jl.git", push_preview=true)
+deploydocs(; repo="github.com/lkdvos/BlockTensorKit.jl.git", push_preview=true)
