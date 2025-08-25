b0bdc9d8ba69bfbf2693efade48770fbf3cedef7
Boris De Vos <boris.devos@ugent.be>
2025-08-25 17:13:18 +0200

    custom `(left/right)oneunit` and `blocksectors` for `SumSpace` of `IsingBimodule` spaces

---
+++ src/vectorspaces/multifusionsumspace.jl
@@ -0,0 +1,39 @@
+# custom methods for SumSpaces
+
+function Base.oneunit(S::SumSpace{Vect[IsingBimodule]})
+    @assert !isempty(S) "Cannot determine type of empty space"
+    allequal(a.row for a in sectors(S)) && allequal(a.col for a in sectors(S)) ||
+        throw(ArgumentError("sectors of $S are not all equal"))
+    first(sectors(S)).row == first(sectors(S)).col || throw(ArgumentError("non-diagonal SumSpace $S"))
+    return SumSpace(oneunit(first(S.spaces)))
+end
+
+function TensorKit.rightoneunit(S::SumSpace{Vect[IsingBimodule]})
+    @assert !isempty(S) "Cannot determine type of empty space"
+    allequal(a.col for a in sectors(S)) || throw(ArgumentError("sectors of $S do not have the same rightone"))
+    return SumSpace(TensorKit.rightoneunit(first(S.spaces)))
+end
+
+function TensorKit.leftoneunit(S::SumSpace{Vect[IsingBimodule]})
+    @assert !isempty(S) "Cannot determine type of empty space"
+    allequal(a.row for a in sectors(S)) || throw(ArgumentError("sectors of $S do not have the same leftone"))
+    return SumSpace(TensorKit.leftoneunit(first(S.spaces)))
+end
+
+function TensorKit.blocksectors(W::TensorMapSpace{SumSpace{Vect[IsingBimodule]},N₁,N₂}) where {N₁,N₂}
+    codom = codomain(W)
+    dom = domain(W)
+    if N₁ == 0 && N₂ == 0
+        return (IsingBimodule(1, 1, 0), IsingBimodule(2, 2, 0)) # should this be a vector?
+    elseif N₁ == 0
+        @assert N₂ != 0 "one of Type IsingBimodule doesn't exist"
+        return filter!(isone, collect(blocksectors(dom)))
+    elseif N₂ == 0
+        @assert N₁ != 0 "one of Type IsingBimodule doesn't exist"
+        return filter!(isone, collect(blocksectors(codom)))
+    elseif N₂ <= N₁ # keep intersection
+        return filter!(c -> TK.hasblock(codom, c), collect(blocksectors(dom)))
+    else
+        return filter!(c -> TK.hasblock(dom, c), collect(blocksectors(codom)))
+    end
+end
