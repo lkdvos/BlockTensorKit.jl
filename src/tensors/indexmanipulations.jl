cc9e53db4274102c562f67eafd3d563e68e61241
Boris De Vos <boris.devos@ugent.be>
2025-11-05 14:39:55 +0100

    renaming from TK v0.15 + incoming renamings

--- src/tensors/indexmanipulations.jl
+++ src/tensors/indexmanipulations.jl
@@ -230,38 +230,38 @@ function TK.add_braid!(
     return TK.add_braid!(tdst, only(tsrc), (p₁, p₂), levels, α, β, backend...)
 end
 
-Base.@constprop :aggressive function TK.insertleftunit(
+Base.@constprop :aggressive function TK.insertleftunitspace(
         t::AbstractBlockTensorMap, i::Int = numind(t) + 1; kwargs...
     )
-    W = TK.insertleftunit(space(t), i; kwargs...)
+    W = TK.insertleftunitspace(space(t), i; kwargs...)
     tdst = similar(t, W)
     for (I, v) in nonzero_pairs(t)
         I′ = CartesianIndex(TT.insertafter(I.I, i - 1, (1,)))
-        tdst[I′] = TK.insertleftunit(v, i; kwargs...)
+        tdst[I′] = TK.insertleftunitspace(v, i; kwargs...)
     end
     return tdst
 end
 
-Base.@constprop :aggressive function TK.insertrightunit(
+Base.@constprop :aggressive function TK.insertrightunitspace(
         t::AbstractBlockTensorMap, i::Int = numind(t) + 1; kwargs...
     )
-    W = TK.insertrightunit(space(t), i; kwargs...)
+    W = TK.insertrightunitspace(space(t), i; kwargs...)
     tdst = similar(t, W)
     for (I, v) in nonzero_pairs(t)
         I′ = CartesianIndex(TT.insertafter(I.I, i, (1,)))
-        tdst[I′] = TK.insertrightunit(v, i; kwargs...)
+        tdst[I′] = TK.insertrightunitspace(v, i; kwargs...)
     end
     return tdst
 end
 
-Base.@constprop :aggressive function TK.removeunit(
+Base.@constprop :aggressive function TK.removeunitspace(
         t::AbstractBlockTensorMap, i::Int; kwargs...
     )
-    W = TK.removeunit(space(t), i)
+    W = TK.removeunitspace(space(t), i)
     tdst = similar(t, W)
     for (I, v) in nonzero_pairs(t)
         I′ = CartesianIndex(TT.deleteat(I.I, i))
-        tdst[I′] = TK.removeunit(v, i)
+        tdst[I′] = TK.removeunitspace(v, i)
     end
     return tdst
 end
