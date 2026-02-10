ddcea5187d24eaa4b94766186e68ad210f47b14c
Lukas Devos <ldevos98@gmail.com>
2026-02-10 09:23:41 -0500

    add VectorInterface specializations

--- src/tensors/vectorinterface.jl
+++ src/tensors/vectorinterface.jl
@@ -39,6 +39,28 @@ end
 
 # add
 # ---
+function VI.add(ty::AbstractBlockTensorMap, tx::AbstractBlockTensorMap, α::Number, β::Number)
+    S = TK.check_spacetype(ty, tx)
+    space(ty) == space(tx) || throw(SpaceMismatch("$(space(ty)) ≠ $(space(tx))"))
+
+    # result type defaults to TensorMap if the types don't match to avoid assymmetric
+    # implementation via zerovector(ty, T) vs zerovector(tx, T)
+    # This would give issues for example with DiagonalTensorMap + TensorMap
+    T = VectorInterface.promote_add(ty, tx, α, β)
+    tdst = if typeof(ty) === typeof(tx)
+        zerovector(ty, T)
+    else
+        M = TK.promote_storagetype(TK.similarstoragetype(ty, T), TK.similarstoragetype(tx, T))
+        if issparse(ty) && issparse(tx)
+            sparseblocktensormaptype(S, numout(ty), numin(ty), M)(undef, space(ty))
+        else
+            blocktensormaptype(S, numout(ty), numin(ty), M)(undef, space(ty))
+        end
+    end
+
+    return add!(scale!(tdst, ty, β), tx, α)
+end
+
 function VI.add!(ty::BlockTensorMap, tx::BlockTensorMap, α::Number, β::Number)
     space(ty) == space(tx) || throw(SpaceMismatch("$(space(ty)) ≠ $(space(tx))"))
     add!(parent(ty), parent(tx), α, β)
