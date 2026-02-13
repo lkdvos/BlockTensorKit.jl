eff92d4c6cc8e236865b4aa3c50ca6da7806020f
Lukas Devos <ldevos98@gmail.com>
2026-02-13 12:01:02 -0500

    type stability improvements (#46)

--- ext/BlockTensorKitAdaptExt.jl
+++ ext/BlockTensorKitAdaptExt.jl
@@ -10,7 +10,9 @@ function Adapt.adapt_structure(to, x::BlockTensorMap)
 end
 
 function Adapt.adapt_structure(to, x::SparseBlockTensorMap)
-    data′ = Dict(I => adapt(to, v) for (I, v) in x.data)
+    ad = adapt(to)
+    TT = Base.promote_op(ad, eltype(x))
+    data′ = Dict{CartesianIndex{ndims(x)}, TT}(I => adapt(to, v) for (I, v) in x.data)
     return SparseBlockTensorMap(data′, space(x))
 end
 
